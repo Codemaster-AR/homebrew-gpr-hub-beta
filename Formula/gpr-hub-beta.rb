00b915d75b9c1c72274ac76f7061a0f1b4455531
Anay Rustogi <codemaster.ar@gmail.com>
2026-02-27 11:14:52 +0400

    v8.0.0 upgrade

--- Formula/gpr-hub-beta.rb
+++ Formula/gpr-hub-beta.rb
@@ -1,8 +1,8 @@
 class GprHubBeta < Formula
   desc "GPR Image Reader and AI Analyzer (Beta)"
   homepage "https://github.com/Codemaster-AR/gpr-hub-cli-beta"
-  url "https://github.com/Codemaster-AR/GPR-Hub-CLI-Beta/archive/refs/tags/v6.0.0-beta.tar.gz"
-  sha256 "9db928f554471c3c00fdabd0d84c571121d027863f5659939a75670579d93344"
+  url "https://github.com/Codemaster-AR/GPR-Hub-CLI-Beta/archive/refs/tags/8.0.0-beta.tar.gz"
+  sha256 "5a1b7277a3b05cd5b39b9c1a85c74d7ed1e7d26a9361b1ce9751fce09dc40592"
   license "MIT"
 
   depends_on "python@3.12"
